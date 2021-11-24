5a5a62a2f34ebd98ff88cd51808852ac1a70d0cb
nothingtosay920 <963717822@qq.com>
2021-11-24 17:54:47 +0800

    完善ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-declare namespace flexible {
-  export const widthUnit = (width: number) => width
-  export const heightUnit = (height: number) => height
-  export const standardUnit = (value: number) => value
+declare module flexible {
+  export const widthUnit = (width: number) => number
+  export const heightUnit = (height: number) => number
+  export const standardUnit = (value: number) => number
 }
